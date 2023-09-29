8d3e0b2a6f5498e7354f76e3bbd2ef3411b7b95b
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2023-09-29 15:21:20 +0200

    Mimics the API of WebSocketServer

--- src/main/java/org/apidesign/polyfill/websocket/WebsocketPolyfill.java
+++ src/main/java/org/apidesign/polyfill/websocket/WebsocketPolyfill.java
@@ -1,14 +1,44 @@
 package org.apidesign.polyfill.websocket;
 
+import java.io.InputStreamReader;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
 
 public final class WebsocketPolyfill {
     private WebsocketPolyfill() {
     }
-    
-    public static void main(String[] args) {
-      try (org.graalvm.polyglot.Context ctx = Context.create()) {
-        ctx.eval("js", "print('Hello world!')");
+
+    public static void prepare(Context ctx) {
+      var code = """
+      function WebSocketServer(config) {
+          var wss = {
+              on : function(type, callback) {
+                 var ws = {
+                    on : function(type, callback) {
+                      print("on ws with " + type + " with " + callback);
+                    }
+                 }
+                 print("on wss with " + type + " with " + callback);
+                 ws.on("connection", callback);
+              }
+          };
+          return wss;
+      }
+      """;
+      var polyfill = Source.newBuilder("js", code, "websocket-polyfill.js")
+          .buildLiteral();
+      ctx.eval(polyfill);
+    }
+
+    public static void main(String[] args) throws Exception {
+      try (
+        var demo = new InputStreamReader(WebsocketPolyfill.class.getResourceAsStream("/WebsocketServerDemo.js"));
+        var ctx = Context.create();
+      ) {
+        prepare(ctx);
+        var src = Source.newBuilder("js", demo, "WebsocketServerDemo.js")
+            .build();
+        ctx.eval(src);
       }
     }
 }
