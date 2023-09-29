1a4072ed2a1f29271599767a84159d56508a9ec8
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2023-09-29 14:50:07 +0200

    mvn -q exec:exec prints JavaScript message

--- src/main/java/org/apidesign/polyfill/websocket/WebsocketPolyfill.java
+++ src/main/java/org/apidesign/polyfill/websocket/WebsocketPolyfill.java
@@ -6,10 +6,8 @@ public final class WebsocketPolyfill {
     private WebsocketPolyfill() {
     }
     
-    public static
-    
     public static void main(String[] args) {
-      try (var ctx = Context.create()) {
+      try (org.graalvm.polyglot.Context ctx = Context.create()) {
         ctx.eval("js", "print('Hello world!')");
       }
     }
